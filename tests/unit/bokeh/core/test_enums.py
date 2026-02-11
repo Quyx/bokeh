3a2dde9dfe505868fb2046acd242190e0ec75ea7
dalthviz <16781833+dalthviz@users.noreply.github.com>
2026-02-11 16:20:26 -0500

    Fix tests. Update baselines

--- tests/unit/bokeh/core/test_enums.py
+++ tests/unit/bokeh/core/test_enums.py
@@ -433,6 +433,7 @@ class Test_bce:
             "unpin",
             "wheel_pan",
             "wheel_zoom",
+            "world",
             "x_box_select",
             "x_box_zoom",
             "x_grip",
