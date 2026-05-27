30bd452c6ba2b6e03441b56808311f88ddae56d7
Jos Heinemann <jos-constantin.heinemann@tu-ilmenau.de>
2026-05-27 03:51:00 +0200

    Added SymLogColorMapper and symlog_cmap function.

--- src/bokeh/transform.py
+++ src/bokeh/transform.py
@@ -36,6 +36,7 @@ from .models.mappers import (
     EqHistColorMapper,
     LinearColorMapper,
     LogColorMapper,
+    SymLogColorMapper,
 )
 from .models.transforms import Dodge, Jitter
 
@@ -63,6 +64,7 @@ __all__ = (
     'jitter',
     'linear_cmap',
     'log_cmap',
+    'symlog_cmap',
     'stack',
     'transform',
 )
@@ -416,6 +418,53 @@ def log_cmap(
     return Field(
         field_name,
         LogColorMapper(
+            palette=palette,
+            low=low,
+            high=high,
+            nan_color=nan_color,
+            low_color=low_color,
+            high_color=high_color,
+        ),
+    )
+
+def symlog_cmap(
+    field_name: str,
+    palette: Sequence[ColorLike],
+    low: float,
+    high: float,
+    low_color: ColorLike | None = None,
+    high_color: ColorLike | None = None,
+    nan_color: ColorLike = "gray",
+) -> Field:
+    ''' Create a ``DataSpec`` dict that applies a client-side ``SymLogColorMapper``
+    transformation to a ``ColumnDataSource`` column.
+
+    Args:
+        field_name (str) : a field name to configure ``DataSpec`` with
+
+        palette (seq[ColorLike]) : a list of colors to use for colormapping
+
+        low (float) : a minimum value of the range to map into the palette.
+            Values below this are clamped to ``low``.
+
+        high (float) : a maximum value of the range to map into the palette.
+            Values above this are clamped to ``high``.
+
+        low_color (ColorLike, optional) : color to be used if data is lower than
+            ``low`` value. If None, values lower than ``low`` are mapped to the
+            first color in the palette. (default: None)
+
+        high_color (ColorLike, optional) : color to be used if data is higher than
+            ``high`` value. If None, values higher than ``high`` are mapped to
+            the last color in the palette. (default: None)
+
+        nan_color (ColorLike, optional) : a default color to use when mapping data
+            from a column does not succeed (default: "gray")
+
+    '''
+    return Field(
+        field_name,
+        SymLogColorMapper(
             palette=palette,
             low=low,
             high=high,
