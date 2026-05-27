52657da458549b65dda8972b55365d97bfa400e8
Jos <jos-constantin.heinemann@tu-ilmenau.de>
2026-05-27 16:52:17 +0200

    Implemented SymLogColorMapper with new scan, index_to_value and value_to_index methods.

--- bokehjs/src/lib/models/mappers/symlog_color_mapper.ts
+++ bokehjs/src/lib/models/mappers/symlog_color_mapper.ts
@@ -5,8 +5,6 @@ import {min, max} from "core/util/arrayable"
 import {clamp} from "core/util/math"
 import type * as p from "core/properties"
 
-// TODO: currently copy of LogColorMapper
-
 export type SymLogScanData = {
   min: number
   max: number
@@ -34,7 +32,7 @@ export class SymLogColorMapper extends ContinuousColorMapper {
     const high = this.high != null ? this.high : max(data)
     const scale = n / (SymLogScale.symlog(high) - SymLogScale.symlog(low))  // subtract the low offset
     const is_reversed = high < low
-    return {max: high, min: low, scale, is_reversed}
+    return { max: high, min: low, scale, is_reversed }
   }
 
   override index_to_value(index: number): number {
