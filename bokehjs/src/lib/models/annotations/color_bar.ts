f38f3d7d358358ae2355f65626fb333d768e8471
Jos <jos-constantin.heinemann@tu-ilmenau.de>
2026-06-10 15:26:46 +0200

    Fixed forgotten color_bar.ts to include SymLog.

--- bokehjs/src/lib/models/annotations/color_bar.ts
+++ bokehjs/src/lib/models/annotations/color_bar.ts
@@ -8,7 +8,7 @@ import {LinearColorMapper, LogColorMapper, ScanningColorMapper, CategoricalColor
 import type {Range} from "../ranges"
 import {Range1d, FactorRange} from "../ranges"
 import type {Scale} from "../scales"
-import {LinearScale, LogScale, LinearInterpolationScale, CategoricalScale} from "../scales"
+import {LinearScale, LogScale, LinearInterpolationScale, CategoricalScale, SymLogScale} from "../scales"
 import type {Ticker} from "../tickers/ticker"
 import {BasicTicker, LogTicker, BinnedTicker, CategoricalTicker, SymLogTicker} from "../tickers"
 import type * as p from "core/properties"
@@ -113,6 +113,8 @@ export class ColorBarView extends BaseColorBarView {
       return new LinearScale()
     } else if (color_mapper instanceof LogColorMapper) {
       return new LogScale()
+    } else if (color_mapper instanceof SymLogColorMapper) {
+      return new SymLogScale()
     } else if (color_mapper instanceof ScanningColorMapper) {
       return new LinearInterpolationScale({binning: this._scanning_binning(color_mapper)})
     } else if (color_mapper instanceof CategoricalColorMapper) {
@@ -127,6 +129,8 @@ export class ColorBarView extends BaseColorBarView {
 
     if (color_mapper instanceof LogColorMapper) {
       return new LogTicker()
+    } else if (color_mapper instanceof SymLogColorMapper) {
+      return new SymLogTicker()
     } else if (color_mapper instanceof ScanningColorMapper) {
       return new BinnedTicker({mapper: color_mapper})
     } else if (color_mapper instanceof CategoricalColorMapper) {
@@ -153,9 +157,9 @@ export class ColorBarView extends BaseColorBarView {
     if (display_high != null) {
       const palette_length = color_mapper.palette.length
       const index_high = color_mapper.value_to_index(display_high, palette_length)
-      if (index_high < palette_length-1) {
+      if (index_high < palette_length - 1) {
         this._index_high = index_high
-        max = color_mapper.index_to_value(index_high+1)
+        max = color_mapper.index_to_value(index_high + 1)
       }
     }
 
@@ -195,7 +199,7 @@ export class ColorBarView extends BaseColorBarView {
         const frame_range = vertical ? this._frame_view.y_range : this._frame_view.x_range
         if (frame_range instanceof Range1d) {
           frame_range.start = binning[0]
-          frame_range.end = binning[binning.length-1]
+          frame_range.end = binning[binning.length - 1]
         }
       }
     } else if (color_mapper instanceof ContinuousColorMapper && range instanceof Range1d) {
@@ -245,7 +249,7 @@ export class ColorBarView extends BaseColorBarView {
     this._index_high = null
     if (display_high != null) {
       const index_high = color_mapper.value_to_index(display_high, binning.length)
-      if (index_high < binning.length-1) {
+      if (index_high < binning.length - 1) {
         this._index_high = index_high
       }
     }
@@ -333,7 +337,7 @@ export namespace ColorBar {
   }
 }
 
-export interface ColorBar extends ColorBar.Attrs {}
+export interface ColorBar extends ColorBar.Attrs { }
 
 export class ColorBar extends BaseColorBar {
   declare properties: ColorBar.Props
@@ -347,10 +351,10 @@ export class ColorBar extends BaseColorBar {
     this.prototype.default_view = ColorBarView
 
     this.define<ColorBar.Props>(({Alpha, Nullable, Float, Ref}) => ({
-      color_mapper: [ Ref(ColorMapper) ],
-      display_low:  [ Nullable(Float), null ],
-      display_high: [ Nullable(Float), null ],
-      scale_alpha:  [ Alpha, 1.0 ],
+      color_mapper: [Ref(ColorMapper)],
+      display_low: [Nullable(Float), null],
+      display_high: [Nullable(Float), null],
+      scale_alpha: [Alpha, 1.0],
     }))
   }
 }
