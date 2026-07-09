4b234ec57bea10fa5cda09d05a37a13a347f3fb8
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2026-07-09 17:29:11 +0200

    revert bokeh_missing_google_api_key_ok

--- docs/bokeh/source/conf.py
+++ docs/bokeh/source/conf.py
@@ -114,7 +114,7 @@ bokeh_example_subdirs = [
     "topics/timeseries",
 ]
 
-bokeh_missing_google_api_key_ok = True
+bokeh_missing_google_api_key_ok = False
 
 if "GOOGLE_API_KEY" not in os.environ:
     print("GOOGLE_API_KEY not found in environment")
